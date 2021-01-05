b84b680486a8d0041258f8f5eec68094c27b91d7
queenanna1999 <72191351+queenanna1999@users.noreply.github.com>
2021-01-06 05:19:21 +1100

    Update main.py

--- main.py
+++ main.py
@@ -79,7 +79,12 @@ async def on_message(message):
         embed.add_field(name='!주사위굴리기', value=' 도리봇이 주사위를 굴려줍니다 ', inline=False)
         embed.add_field(name='!유튜버 추천', value=' 도리봇이 검증된 유튜버를 추천해줍니다 ', inline=False)
         embed.add_field(name='!MBTI', value=' 도리봇이 MBTI끼리의 궁합을 불러옵니다 ', inline=False) 
-        embed.add_field(name='!혈액형', value=' 도리봇이 혈액형의 특징,혈액형끼리의 궁합을 불러옵니다 ', inline=False)        
+        embed.add_field(name='!혈액형', value=' 도리봇이 혈액형의 특징,혈액형끼리의 궁합을 불러옵니다 ', inline=False)
+        embed.add_field(name='!자살이란', value=' 도리봇이 자살의 정의들을 불러옵니다. 해당 문구는 랜덤으로 한개씩 출력됩니다 ', inline=False)
+        embed.add_field(name='!음악', value=' [미완성]도리봇을 음성채널에 추가 및 음악 재생 (예시: !음악 https://www.youtube.com/watch?v=8G0xVM-X9Jk&list=WL&index=4 ) ', inline=False)
+        embed.add_field(name='!종료', value=' [미완성]도리봇을 음성채널에서 내보내기 ', inline=False)
+        embed.add_field(name='!스킵', value=' [미완성]도리봇이 음악을 스킵합니다 ', inline=False)
+        embed.add_field(name='!목록', value=' [미완성]도리봇이 재생목록을 불러옵니다 ', inline=False)
         embed.add_field(name='!자기소개', value=' 도리봇이 제작자의 자기소개를 대신 무료로 해줍니다 ', inline=False)
         
         await message.channel.send(channel,embed=embed)
@@ -1653,7 +1658,22 @@ async def on_message(message):
             await message.channel.send("뒷문 밖에는 갈잎의 노래")
             await message.channel.send("엄마야 누나야 강변 살자")
             
-            
+        if message.content.startswith("!자살"):       #추가바람.
+            randomNum = random.randrange(1, 7)
+            if randomNum==1:
+                await message.channel.send(embed=discord.Embed(title="'자살은 스스로 품은 의지를 통해 자기 생명을 해쳐서 죽음이라는 결과에 이르는 자멸 행위이다.' -세계보건기구-", color=0x00ff00))
+            if randomNum==2:
+                await message.channel.send(embed=discord.Embed(title="'희생자 자신이 결과를 알면서도 적극적, 소극적 행동으로 직접, 또는 타인을 통해 행하는 죽음을 자살이라고 부른다.' -에밀 뒤르켐(1897)-", color=0x00ff00))
+            if randomNum==3:
+                await message.channel.send(embed=discord.Embed(title="'사망자 자신이 희생이 아닌 어떠한 의도를 가지고 혹은 죽음을 위해 행한 행위가 일으킨 죽음을 모두 자살이라고 부른다.' -알버크(1930)-", color=0x00ff00))
+            if randomNum==4:
+                await message.channel.send(embed=discord.Embed(title="'자살은 삶을 선택할 수도 있겠지만 모든 사회적 의무에서 벗어나기 위해 죽음을 고른 명석한 인간이 행한 행위를 말한다.' -아킬 델마(1932)-", color=0x00ff00))
+            if randomNum==5:
+                await message.channel.send(embed=discord.Embed(title="'자살은 죽음을 수단이나 결과로 여겨 스스로 죽는 행위다.' -드에(1947)-", color=0x00ff00))
+            if randomNum==6:
+                await message.channel.send(embed=discord.Embed(title="'자살은 실존에 관한 문제를 해결하는 방법을 주체의 자발적인 죽음에서 구하고 찾는 일이다.' -바에슐러(1975)-", color=0x00ff00))
+          
+          
     if message.content.startswith("!MBTI") or message.content.startswith('MBTI') or message.content.startswith('엠비티아이') or message.content.startswith('성격') or message.content.startswith('!mbti') or message.content.startswith('mbti'):
         await message.channel.send("당신의 MBTI 를 채팅창에 입력해주세요.(무조건, 대문자로만 입력해주세요, 대문자만 인식합니다.) ")
         await message.channel.send("예시) !ENFP (느낌표 + ENFP) ")
@@ -1677,6 +1697,105 @@ async def on_message(message):
         await message.channel.send(embed=embed)
         msg = "-끝-{0.author.mention} ".format(message)
         await message.channel.send( msg)
-        
+
+      
+app = discord.Client()
+que = {}
+playerlist = {}
+playlist = list() #재생목록 리스트
+
+def queue(id): #음악 재생용 큐
+        if que[id] != []:
+                player = que[id].pop(0)
+	              	playerlist[id] = player
+		              del playlist[0]
+	              	player.start()
+
+@app.event 
+async def on_ready(): #봇이 준비되었을 때 출력할 내용
+       	print(app.user.name)
+       	print(app.user.id)
+
+@app.event
+async def on_message(message):
+
+	       if message.author == app.user: #봇이 채팅을 쳤을 때 명령어로 인식되지 않음
+	              	return
+
+
+       	if message.content.startswith("!음악"): #음성채널에 봇을 추가 및 음악 재생
+		              msg = message.content.split(" ")
+		              try:
+			                     url = msg[1]
+			                     url1 = re.match('(https?://)?(www\.)?((youtube\.(com))/watch\?v=([-\w]+)|youtu\.be/([-\w]+))', url) #정규 표현식을 사용해 url 검사
+		                     	if url1 == None:
+			                            	await message.channel.send(embed=discord.Embed(title=":no_entry_sign: url을 제대로 입력해주세요."))
+			                            	return
+	              	except IndexError:
+			                     await message.channel.send(embed=discord.Embed(title=":no_entry_sign: url을 입력해주세요."))
+			                     return
+
+		              channel = message.author.voice.voice_channel 
+		              server = message.server
+	              	voice_client = app.voice_client_in(server)
+
+	              	if app.is_voice_connected(server) and not playerlist[server.id].is_playing(): #봇이 음성채널에 접속해있으나 음악을 재생하지 않을 때
+		                     	await voice_client.disconnect()
+	              	elif app.is_voice_connected(server) and playerlist[server.id].is_playing(): #봇이 음성채널에 접속해있고 음악을 재생할 때
+		                     	player = await voice_client.create_ytdl_player(url,after=lambda:queue(server.id),before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+		                     	if server.id in que: #큐에 값이 들어있을 때
+			                            	que[server.id].append(player)
+		                     	else: #큐에 값이 없을 때
+			                            	que[server.id] = [player]
+		                     	await message.channel.send(embed=discord.Embed(title=":white_check_mark: 추가 완료!"))
+		                     	playlist.append(player.title) #재생목록에 제목 추가
+		                     	return
+
+	              	try:
+			                     voice_client = await app.join_voice_channel(channel)
+	              	except discord.errors.InvalidArgument: #유저가 음성채널에 접속해있지 않을 때
+		                     	await message.channel.send(embed=discord.Embed(title=":no_entry_sign: 음성채널에 접속하고 사용해주세요."))
+			                     return
+
+	              	try:
+		                     	player = await voice_client.create_ytdl_player(url,after=lambda:queue(server.id),before_options="-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5")
+			                     playerlist[server.id] = player
+		                     	playlist.append(player.title)
+	              	except youtube_dl.utils.DownloadError: #유저가 제대로 된 유튜브 경로를 입력하지 않았을 때
+			                     await message.channel.send(embed=discord.Embed(title=":no_entry_sign: 존재하지 않는 경로입니다."))
+		                     	await voice_client.disconnect()
+		                     	return
+		              player.start()
+
+	       if message.content == "!종료": #음성채널에서 봇을 나가게 하기
+	              	server = message.server
+	              	voice_client = client.voice_client_in(server)
+
+		              if voice_client == None: #봇이 음성채널에 접속해있지 않았을 때
+		                     	await message.channel.send(embed=discord.Embed(title=":no_entry_sign: 봇이 음성채널에 없어요."))
+		                     	return
+		
+		              await message.channel.send(embed=discord.Embed(title=":mute: 채널에서 나갑니다.")) #봇이 음성채널에 접속해있을 때
+		              await voice_client.disconnect()
+
+        	if message.content == "!스킵":
+	              	id = message.server.id
+		              if not playerlist[id].is_playing(): #재생 중인 음악이 없을 때
+		                     	await message.channel.send(embed=discord.Embed(title=":no_entry_sign: 스킵할 음악이 없어요."))
+			                     return
+		              await clmessage.channel.send(embed=discord.Embed(title=":mute: 스킵했어요."))
+		              playerlist[id].stop()
+	
+        	if message.content == "!목록":
+
+		               if playlist == []:
+			                      await message.channel.send(embed=discord.Embed(title=":no_entry_sign: 재생목록이 없습니다."))
+			                      return
+
+		               playstr = "```css\n[재생목록]\n\n"
+		               for i in range(0, len(playlist)):
+			                      playstr += str(i+1)+" : "+playlist[i]+"\n"
+		               await message.channel.send(playstr+"```")   
+           
 accross_token = os.environ["BOT_TOKEN"]
 app.run(accross_token)
